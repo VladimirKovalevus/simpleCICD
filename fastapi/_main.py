fd5ca023915a123c80461078189d3bab70c0cfff
Maksim Boldyrev <maksbold2015@gmail.com>
2023-10-15 17:52:46 +0700

    add: fastapi send_sms

--- fastapi/_main.py
+++ fastapi/_main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
+import requests
 
 class GitInfo(BaseModel):
     owner: str
-    payplan: str
+    payload: str
     status: bool
 
 app = FastAPI(
@@ -13,5 +14,11 @@ app = FastAPI(
 @app.post('/git/', tags=['Git']) 
 async def create_workspace(info: GitInfo, Autorization: str | None = Header(default=None)):
     if Autorization == "123": 
+        send_telegram(str(info));
         return {"info": info, "Autorization": Autorization}
     raise HTTPException(status_code = 401, detail="Unauthorized")
+
+def send_telegram(payload: str):
+    url = "https://www.kunteynir.space/submitpayload"
+    user_id = 828170828
+    response = requests.get(url + "user_id={user_id}&payload={payload}")
