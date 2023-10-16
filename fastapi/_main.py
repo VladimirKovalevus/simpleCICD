609bfd5668d8e1dec5df30e7621b6b8742f5bd7e
Maksim Boldyrev <maksbold2015@gmail.com>
2023-10-16 20:15:14 +0700

    privet

--- fastapi/_main.py
+++ fastapi/_main.py
@@ -14,11 +14,11 @@ app = FastAPI(
 @app.post('/git/', tags=['Git']) 
 async def create_workspace(info: GitInfo, Autorization: str | None = Header(default=None)):
     if Autorization == "123": 
-        send_telegram(str(info));
+        # send_telegram(str(info));
         return {"info": info, "Autorization": Autorization}
     raise HTTPException(status_code = 401, detail="Unauthorized")
 
-def send_telegram(payload: str):
-    url = "https://www.kunteynir.space/submitpayload"
-    user_id = 828170828
-    response = requests.get(url + "user_id={user_id}&payload={payload}")
+# def send_telegram(payload: str):
+#     url = "https://www.kunteynir.space/submitpayload"
+#     user_id = 828170828
+#     response = requests.get(url + "user_id={user_id}&payload={payload}")
